4fc29f8c521225fa40d66be55ada7b2174c984db
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-03-15 07:05:12 -0500

    Version 1.0 MVP

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let container = document.getElementById("container");
 
-let htmlMd = () => {
+let render = (file) => {
   let main = document.createElement("div");
+
   container.innerHTML = "";
   main.innerHTML = `
-  <zero-md src='/notes/HTML.md'>
+  <zero-md src='/notes/${file}.md'>
+    <template>
+         <link rel="stylesheet" href="theme.css">
+    </template>
+  </zero-md>
   `;
   container.appendChild(main);
 };
