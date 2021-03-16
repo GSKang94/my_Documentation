1ae4681cd3009ff5c0ae7a05f6cffbb808260ba6
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-03-16 09:22:05 -0500

    Fixed responsive bugs

--- app.js
+++ app.js
@@ -12,32 +12,36 @@ let render = (file) => {
   </zero-md>
   `;
 
+  // fade effect on page change
   main.animate([{ opacity: 0 }, { opacity: 1 }], 500);
 
   container.appendChild(main);
 };
 
-// Hide nav on scroll
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector("nav").style.top = "0";
-  } else {
-    document.querySelector("nav").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+let mql = window.matchMedia("(max-width: 480px)");
+if (mql.matches) {
+  // Hide nav on scroll
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector("nav").style.top = "0";
+    } else {
+      document.querySelector("nav").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+}
 
-let home = () => {
-  container.innerHTML = "";
-  let home = document.createElement("div");
-  home.innerHTML = `
-<div id="home">
-<h1>my</h1>
-</div>
-`;
-};
+// let home = () => {
+//   container.innerHTML = "";
+//   let home = document.createElement("div");
+//   home.innerHTML = `
+// <div id="home">
+// <h1>my</h1>
+// </div>
+// `;
+// };
 
 // let navLinks = document.querySelector("ul");
 // const links = document.querySelectorAll("li");
