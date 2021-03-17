2e10cf59d3bf6fffbf91bb946822c9f2cebd4b9c
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-03-16 20:08:13 -0500

    Added active class on user-select

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-let container = document.getElementById("container");
+const links = document.querySelectorAll("li");
+// let navLinks = document.querySelector("body nav");
+links.forEach((li) => {
+  li.addEventListener("click", () => {
+    for (let sibling of li.parentNode.children) {
+      if (sibling !== li) {
+        sibling.classList.remove("active");
+      } else {
+        li.classList.add("active");
+      }
+    }
 
-let render = (file) => {
-  let main = document.createElement("div");
+    let main = document.createElement("div");
+    let liInnerText = li.textContent.replace(/\s{2,}/g, " ").trim();
+    let container = document.getElementById("container");
 
-  container.innerHTML = "";
-  main.innerHTML = `
-  <zero-md src='assets/notes/${file}.md'>
-    <template>
-         <link rel="stylesheet" href="assets/theme.css">
-    </template>
-  </zero-md>
-  `;
+    container.innerHTML = "";
+    main.innerHTML = `
+    <zero-md src='assets/notes/${liInnerText}.md'>
+      <template>
+           <link rel="stylesheet" href="assets/theme.css">
+      </template>
+    </zero-md>
+    `;
 
-  // fade effect on page change
-  main.animate([{ opacity: 0 }, { opacity: 1 }], 500);
+    // fade effect on page change
+    main.animate([{ opacity: 0 }, { opacity: 1 }], 500);
 
-  container.appendChild(main);
-};
+    container.appendChild(main);
+  });
+});
 
-let mql = window.matchMedia("(max-width: 480px)");
-if (mql.matches) {
-  // Hide nav on scroll
+// Hide nav on scroll on mobile
+let phoneSize = window.matchMedia("(max-width: 480px)");
+if (phoneSize.matches) {
   var prevScrollpos = window.pageYOffset;
   window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
@@ -43,8 +55,6 @@ if (mql.matches) {
 // `;
 // };
 
-// let navLinks = document.querySelector("ul");
-// const links = document.querySelectorAll("li");
 // let hamburger = document.getElementById("ham-burger");
 // console.log(links.length);
 // hamburger.addEventListener("click", () => {
